f92d33bb390324d3498ea371dd61e6a74f1dfc7d
qian-gao <49657171+qian-gao@users.noreply.github.com>
2023-05-21 19:42:04 +0200

    update render function

--- R/render_rmarkdown.R
+++ R/render_rmarkdown.R
@@ -1,13 +1,28 @@
+#' @title render_rmarkdown
+#'
+#' @description Provides an overview table for the time and scope conditions of
+#'     a data set
+#'
+#' @param file = NULL,
+#' @param params = NULL,
+#' @param output = NULL
+#'
+#' @return A data frame object that contains a summary of a sample that
+#'     can later be converted to a TeX output using \code{overview_print}
+#' @examples
+#'
+#' @export
+
 render_rmarkdown <-
   function(
     file = NULL,
     params = NULL,
     output = NULL
-    
+
   ){
-    
+
     rmarkdown::render( file,
                        params = params,
                        output_file = paste0( output, "_", Sys.Date(), ".html"))
-  
+
    }
