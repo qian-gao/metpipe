363ba2455842e5c3e0ad11cb5b56e0c1750d12c3
qian-gao <49657171+qian-gao@users.noreply.github.com>
2023-07-19 14:03:44 +0200

    Update get_lipid_from_lipidmaps.R

--- R/get_lipid_from_lipidmaps.R
+++ R/get_lipid_from_lipidmaps.R
@@ -20,57 +20,55 @@ get_lipid_from_lipidmaps <-
             type = "abbrev", # "abbrev" "abbrev_chains"
             keep.all = FALSE
   ){
-    
-    lipids <- lip.names$Compound.name
-    
+
     df <-
       data.frame(lipid = lipids) %>%
       mutate( query = gsub(" ", "(", lipid),
               query = paste0(query, ")") )
-    
+
     temp <- paste0("www.lipidmaps.org/rest/compound/", type, "/")
-    
+
     info <-
       apply(df, 1, function(x){
-        
+
         all <- httr::content(httr::GET(url = paste0(temp, x[["query"]], "/all")))
-        
-        
+
+
         if ("input" %in% names(all)) {
-          
-          df.i <- 
+
+          df.i <-
             as.data.frame(all) %>%
             mutate(isomer_label = FALSE)
-          
+
         } else {
-          
+
           if (keep.all){
-          
-            df.i <- 
+
+            df.i <-
               data.table::rbindlist(all, fill = TRUE) %>%
               mutate(isomer_label = TRUE)
-            
+
           } else {
-            
-            df.i <- 
-              data.table::rbindlist(all, fill = TRUE)[1, ] 
-            
+
+            df.i <-
+              data.table::rbindlist(all, fill = TRUE)[1, ]
+
             df.i$isomer_label = TRUE
-            
+
           }
-          
+
         }
-        
+
         return(df.i)
-        
+
       })
-    
+
     result <- data.table::rbindlist(info, fill = TRUE)
-    
+
     output <-
       df %>%
       left_join(result, by = c("query" = "input"))
-    
+
     return(output)
-    
+
   }
