7a276b21d9b62622fd0f11938fbd2743efce539b
qian-gao <49657171+qian-gao@users.noreply.github.com>
2025-10-04 18:37:26 +0200

    add argument for prepare_workflow

--- inst/prepare_run_workflow.R
+++ inst/prepare_run_workflow.R
@@ -2,27 +2,39 @@
 
 # Input
 args <- commandArgs(trailingOnly = TRUE)
-path_raw <- args[1]
-path_temp <- args[2]
+
+get_arg <- function(name) {
+  i <- which(args == name)
+  if (length(i) == 0) return(NULL)
+  return(args[i + 1])
+}
+
+path_raw <- get_arg("--raw")
+path_temp <- get_arg("--temp")
 
 defaults <- list(
   path_result = gsub("raw", "peaktable", path_raw),
   path_workflow = "/wd",
-  path_mzmine = "/wd/mzmine_linux/bin/mzmine"
+  path_mzmine = "/wd/mzmine_linux/bin/mzmine",
+  path_yaml = system.file("scripts", "prep_yaml.Rmd", package = "metpipe")
 )
 
-path_workflow <- ifelse(length(args) >= 3, args[3], defaults$path_workflow)
-path_mzmine <- ifelse(length(args) >= 4, args[4], defaults$path_mzmine)
-path_result <- ifelse(length(args) >= 5, args[5], defaults$path_result)
+path_workflow <- dplyr::coalesce(get_arg("--workflow"), defaults$path_workflow)
+path_mzmine <- dplyr::coalesce(get_arg("--mzmine"), defaults$path_mzmine)
+path_result <- dplyr::coalesce(get_arg("--result"), defaults$path_result)
+path_yaml <- dplyr::coalesce(get_arg("--yaml"), defaults$path_yaml)
 
 # Prepare workflow
 prepare_workflow <- system.file("prepare_workflow.R", package = "metpipe")
+
 system(paste0("Rscript ", prepare_workflow, 
-              " ", path_raw, 
-              " ", path_temp,
-              " ", path_workflow,
-              " ", path_mzmine,
-              " ", path_result))
+              " --raw ", path_raw, 
+              " --temp ", path_temp
+              # " --workflow ", path_workflow,
+              # " --mzmine ", path_mzmine,
+              # " --result ", path_result,
+              # " --yaml ", path_yaml
+))
 
 # Run workflow
 config_file <- paste0(path_result, "/config.yml")
--- inst/prepare_workflow.R
+++ inst/prepare_workflow.R
@@ -2,24 +2,33 @@
 
 # Input
 args <- commandArgs(trailingOnly = TRUE)
-path_raw <- args[1]
-path_temp <- args[2]
+
+get_arg <- function(name) {
+  i <- which(args == name)
+  if (length(i) == 0) return(NULL)
+  return(args[i + 1])
+}
+
+path_raw <- get_arg("--raw")
+path_temp <- get_arg("--temp")
 
 defaults <- list(
   path_result = gsub("raw", "peaktable", path_raw),
   path_workflow = "/wd",
-  path_mzmine = "/wd/mzmine_linux/bin/mzmine"
+  path_mzmine = "/wd/mzmine_linux/bin/mzmine",
+  path_yaml = system.file("scripts", "prep_yaml.Rmd", package = "metpipe")
 )
 
-path_workflow <- ifelse(length(args) >= 3, args[3], defaults$path_workflow)
-path_mzmine <- ifelse(length(args) >= 4, args[4], defaults$path_mzmine)
-path_result <- ifelse(length(args) >= 5, args[5], defaults$path_result)
+path_workflow <- dplyr::coalesce(get_arg("--workflow"), defaults$path_workflow)
+path_mzmine <- dplyr::coalesce(get_arg("--mzmine"), defaults$path_mzmine)
+path_result <- dplyr::coalesce(get_arg("--result"), defaults$path_result)
+path_yaml <- dplyr::coalesce(get_arg("--yaml"), defaults$path_yaml)
 
 # Configuration
 if (!dir.exists(path_result)) dir.create(path_result)
 
 rmarkdown::render(
-  input = system.file("scripts", "prep_yaml.Rmd", package = "metpipe"),
+  input = path_yaml,
   #input = paste0(path_workflow, "/scripts/prep_yaml.Rmd"),
   params = list(path_raw = path_raw,
                 path_result = path_result,
