8e1f45a05f88b72a71543ce796fb73e7f9b55bd8
qian-gao <49657171+qian-gao@users.noreply.github.com>
2025-11-06 16:17:00 +0100

    Update map_standard_name_to_meta.R

--- R/map_standard_name_to_meta.R
+++ R/map_standard_name_to_meta.R
@@ -9,7 +9,8 @@
 #' @examples
 map_standard_name_to_meta <- 
   function(raw_files,
-           qc_types = c("BL", "NIST", "PO", "sol", "CP", "IQ", "BPL", "MMix")){
+           qc_types = c("BL", "NIST", "PO", "sol", "CP", "IQ", "BPL", "MMix"),
+           pasef_as_dda = FALSE){
     
     File.name <- basename(raw_files)
     Run.order <- as.numeric(gsub(".d|.mzML", "", str_extract(File.name, "[0-9]+.d$|[0-9]+.mzML$")))
@@ -29,32 +30,64 @@ map_standard_name_to_meta <-
     colnames(file) <-
       c("File.name", "Project", "Method", "Date", "Run.seq", "Sample", "Extract.rep", "Tech.rep" , "Run.order")
     
-    file <- 
-      file %>% 
-      mutate(Sample.type = case_when(grepl("DDA", Method) ~ "DDA",
-                                     Sample %in% qc_types ~ Sample,
-                                     TRUE ~ "Sample"),
-             #Sample.seq = row_number(),
-             Sample = case_when(Sample.type %in% qc_types ~ paste(Sample, Extract.rep, Tech.rep, sep = "_"),
-                                Sample.type == "DDA" ~ paste("DDA", Sample, Extract.rep, Tech.rep, sep = "_"),
-                                TRUE ~ Sample),
-             Run.order = dense_rank(Run.order)) %>% 
-      arrange(Run.order) %>% 
-      mutate(Sample.batch = paste0("Batch ", row_number() %/% 100 + 1)) %>% 
-      #arrange(Sample.seq) %>% 
-      group_by(Sample) %>% 
-      mutate(n = n(),
-             Sample = ifelse(n > 1, 
-                             paste0(Sample, "_", Tech.rep),
-                             Sample)) %>%
-      ungroup() %>% 
-      group_by(Sample) %>% 
-      mutate(n = n(),
-             Sample = ifelse(n > 1, 
-                             paste0(Sample, "-", row_number()),
-                             Sample)) %>%
-      ungroup() %>% 
-      select(-n) 
+    if (pasef_as_dda){
+      
+      file <- 
+        file %>% 
+        mutate(Sample.type = case_when(grepl("PASEF|DDA", Method) ~ "DDA",
+                                       Sample %in% qc_types ~ Sample,
+                                       TRUE ~ "Sample"),
+               #Sample.seq = row_number(),
+               Sample = case_when(Sample.type %in% qc_types ~ paste(Sample, Extract.rep, Tech.rep, sep = "_"),
+                                  Sample.type == "DDA" ~ paste("DDA", Sample, Extract.rep, Tech.rep, sep = "_"),
+                                  TRUE ~ Sample),
+               Run.order = dense_rank(Run.order)) %>% 
+        arrange(Run.order) %>% 
+        mutate(Sample.batch = paste0("Batch ", row_number() %/% 100 + 1)) %>% 
+        #arrange(Sample.seq) %>% 
+        group_by(Sample) %>% 
+        mutate(n = n(),
+               Sample = ifelse(n > 1, 
+                               paste0(Sample, "_", Tech.rep),
+                               Sample)) %>%
+        ungroup() %>% 
+        group_by(Sample) %>% 
+        mutate(n = n(),
+               Sample = ifelse(n > 1, 
+                               paste0(Sample, "-", row_number()),
+                               Sample)) %>%
+        ungroup() %>% 
+        select(-n)
+      
+    } else {
+      
+      file <- 
+        file %>% 
+        mutate(Sample.type = case_when(grepl("DDA", Method) ~ "DDA",
+                                       Sample %in% qc_types ~ Sample,
+                                       TRUE ~ "Sample"),
+               #Sample.seq = row_number(),
+               Sample = case_when(Sample.type %in% qc_types ~ paste(Sample, Extract.rep, Tech.rep, sep = "_"),
+                                  Sample.type == "DDA" ~ paste("DDA", Sample, Extract.rep, Tech.rep, sep = "_"),
+                                  TRUE ~ Sample),
+               Run.order = dense_rank(Run.order)) %>% 
+        arrange(Run.order) %>% 
+        mutate(Sample.batch = paste0("Batch ", row_number() %/% 100 + 1)) %>% 
+        #arrange(Sample.seq) %>% 
+        group_by(Sample) %>% 
+        mutate(n = n(),
+               Sample = ifelse(n > 1, 
+                               paste0(Sample, "_", Tech.rep),
+                               Sample)) %>%
+        ungroup() %>% 
+        group_by(Sample) %>% 
+        mutate(n = n(),
+               Sample = ifelse(n > 1, 
+                               paste0(Sample, "-", row_number()),
+                               Sample)) %>%
+        ungroup() %>% 
+        select(-n) 
+    }
     
     return(file)
     
